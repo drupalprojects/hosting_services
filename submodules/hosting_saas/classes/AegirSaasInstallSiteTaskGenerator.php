8c247f0e5c00a50ea4d2177e06ba7dc77c13a5b9
Colan Schwartz <colan@58704.no-reply.drupal.org>
2016-09-21 13:27:20 -0400

    Issue #2771765 by colan: Allow for site installation on task creation requests.

--- submodules/hosting_saas/classes/AegirSaasInstallSiteTaskGenerator.php
+++ submodules/hosting_saas/classes/AegirSaasInstallSiteTaskGenerator.php
@@ -9,7 +9,7 @@ class AegirSaasInstallSiteTaskGenerator extends AegirSaasCreateSiteTaskGenerator
    * {@inheritdoc}
    */
   public function setSiteIdAndVerify() {
-    if ($this->site = hosting_get_site_by_url($this->site, FALSE)) {
+    if (hosting_get_site_by_url($this->site, FALSE)) {
       $message = "Cannot populate task: Site name matches an existing site. A new name must be provided.";
       $this->logErrorAndThrowException($message);
     }
